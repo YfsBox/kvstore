fa0eb7d30d448675602ef24c4fafd3d591958d38
YfsBox <1027342142@qq.com>
2023-03-01 20:39:02 +0800

    feat(SkipList.h/cc): add Node

--- src/SkipList.h
+++ src/SkipList.h
@@ -5,12 +5,46 @@
 #ifndef KVSTORE_SKIPLIST_H
 #define KVSTORE_SKIPLIST_H
 
+#include <cassert>
+#include <iostream>
+#include <vector>
 #include "KvContainer.h"
 
 namespace kvstore {
+
+    template<class KEY, class VALUE>
+    struct Node {
+        using NodePtr = Node*;
+
+        const KEY key_;
+
+        VALUE value_;
+
+        Node(const KEY &key, const VALUE &value, int level): key_(key), value_(value) {
+            nexts_.reserve(level);
+        }
+
+        ~Node() = default;
+
+        NodePtr GetNext(int n) {
+            assert(n >= 0);
+            return nexts_[n];
+        }
+
+        void SetNext(int n, const NodePtr node) {
+            assert(n >= 0);
+            nexts_[n] = node;
+        }
+
+    private:
+        std::vector<NodePtr> nexts_;
+    };
+
     template<class KEY, class VALUE>
     class SkipList: public KvContainer<KEY, VALUE> {
     public:
+        using NodePtr = Node<KEY, VALUE>;
+
         SkipList();
 
         ~SkipList();
@@ -26,8 +60,10 @@ namespace kvstore {
         }
 
     private:
+        static constexpr uint8_t kMaxHeight = 12;
 
-
+        NodePtr header_;
+        uint8_t curr_height_{1};
     };
 
 
