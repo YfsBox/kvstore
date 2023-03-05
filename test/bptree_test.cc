43f80135fdb5e389aeeb1ec92c41fb6567f56cf4
YfsBox <1027342142@qq.com>
2023-03-05 15:47:11 +0800

    feat(BPlusTree.h/cc):增加bptree_test.cc

---
+++ test/bptree_test.cc
@@ -0,0 +1,19 @@
+//
+// Created by 杨丰硕 on 2023/3/5.
+//
+#include <gtest/gtest.h>
+#include "../src/BPlusTree.h"
+
+using namespace kvstore;
+
+TEST(BPTREE_TEST, BASIC_TEST) {
+    // TODO: write test code for b+ tree
+
+}
+
+
+int main(int argc, char* argv[]) {
+    testing::InitGoogleTest(&argc, argv);
+    RUN_ALL_TESTS();
+    return 0;
+}
